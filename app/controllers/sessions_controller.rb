34e0bfb44a60c90c8ee2b7c83816b522cf0cf22e
daikinakaji <dikibaseball0421@icloud.com>
2018-12-14 21:46:28 +0900

    initial commit

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,28 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user, success: "ログインに成功しました"
+    else
+      flash.now[:danger] = "ログインに失敗しました"
+      render :new
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
+    #code
+  end
+
+  private
+  def  log_in(user)
+    session[:user_id] = user.id
+  end
+
+
+end
